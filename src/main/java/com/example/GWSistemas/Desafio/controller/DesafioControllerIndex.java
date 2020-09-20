31c1e966d8df69addd58a7147cdc82a8c4bc4299
LucasSalesMoreira <lucasdevsoftware@gmail.com>
2020-09-20 19:04:37 -0300

    init

---
+++ src/main/java/com/example/GWSistemas/Desafio/controller/DesafioControllerIndex.java
@@ -0,0 +1,15 @@
+package com.example.GWSistemas.Desafio.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DesafioControllerIndex {
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public ResponseEntity<String> postCreate() {
+        return ResponseEntity.ok("OK!");
+    }
+}
