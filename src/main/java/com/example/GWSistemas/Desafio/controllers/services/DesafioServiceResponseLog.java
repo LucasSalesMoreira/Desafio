53cf9db493b036f9af6ba4396b671574ec054cab
LucasSalesMoreira <lucasdevsoftware@gmail.com>
2020-09-23 16:06:01 -0300

    Montando JsonObject de response

--- src/main/java/com/example/GWSistemas/Desafio/controllers/services/DesafioServiceResponseLog.java
+++ src/main/java/com/example/GWSistemas/Desafio/controllers/services/DesafioServiceResponseLog.java
@@ -1,2 +1,20 @@
-package com.example.GWSistemas.Desafio.controllers.services;public class DesafioServiceResponseLog {
+package com.example.GWSistemas.Desafio.controllers.services;
+
+import com.example.GWSistemas.Desafio.controllers.services.repositorys.DesafioRepositoryResponseLog;
+import com.example.GWSistemas.Desafio.models.DesafioModelUser;
+import com.google.gson.JsonObject;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DesafioServiceResponseLog {
+    public JsonObject generateResponse(DesafioModelUser user) {
+
+        JsonObject log = new DesafioRepositoryResponseLog().getResponseLog(user.getEmail());
+        log.addProperty("nome", user.getNome());
+        log.addProperty("email", user.getEmail());
+        log.addProperty("senha", new DesafioServiceSecurity().encrypt(user.getSenha()));
+        log.add("telefones", user.getTelefones());
+
+        return log;
+    }
 }
