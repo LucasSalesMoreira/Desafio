d5fe8915fb96f4dbfbfa52154657e2dd3c1a2fbd
LucasSalesMoreira <lucasdevsoftware@gmail.com>
2020-09-24 16:45:31 -0300

    Casse para armazenar os stutus_token padronizados pela API

---
+++ src/main/java/com/example/GWSistemas/Desafio/controllers/services/repositorys/DesafioRepositoryTokenStatus.java
@@ -0,0 +1,9 @@
+package com.example.GWSistemas.Desafio.controllers.services.repositorys;
+
+public class DesafioRepositoryTokenStatus {
+    public static final String _VALID_TOKEN = "001";
+    public static final String _UPDATED_BY_EXPIRATION = "002";
+    public static final String _UPDATED_BY_INVALIDATION = "003";
+    public static final String _INCORRECT_TOKEN = "004";
+    public static final String _ERROR = "005";
+}
