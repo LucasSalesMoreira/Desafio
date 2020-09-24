ca3eb1404bf7b376c29c36eeeec5b14efffe7a73
LucasSalesMoreira <lucasdevsoftware@gmail.com>
2020-09-24 16:46:38 -0300

    Casse para fazer a persistencia do novo token gerado.

--- src/main/java/com/example/GWSistemas/Desafio/controllers/services/repositorys/DesafioRepositoryTokenUpdate.java
+++ src/main/java/com/example/GWSistemas/Desafio/controllers/services/repositorys/DesafioRepositoryTokenUpdate.java
@@ -1,2 +1,31 @@
-package com.example.GWSistemas.Desafio.controllers.services.repositorys;public class DesafioRepositoryUpdateToken {
+package com.example.GWSistemas.Desafio.controllers.services.repositorys;
+
+import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@Repository
+public class DesafioRepositoryTokenUpdate {
+
+    public boolean update(String id, String token) {
+
+        try {
+            String sql = "update log set token = ? where id_user = ?";
+            DesafioRepositoryConnection desafioConnection = new DesafioRepositoryConnection();
+            Connection conn = desafioConnection.connect();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, token);
+            stmt.setString(2, id);
+            stmt.executeUpdate();
+            desafioConnection.finish(conn, stmt);
+            return true;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
 }
